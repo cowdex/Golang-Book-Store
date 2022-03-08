d79a94b73e585aca53097efc92595bc46ee46d9d
Mohamed Dhik <92504802+mohamedhik@users.noreply.github.com>
2022-03-09 00:16:04 +0100

    db connection

---
+++ pkg/config/app.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm/tree/master/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connections() {
+	d, err := gorm.Open("mysql", "dhik:Dhik@2000/simplerest?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
